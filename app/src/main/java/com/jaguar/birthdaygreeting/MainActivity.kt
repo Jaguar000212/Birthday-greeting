4f71e2aebf374b961ae24c0c98155a6381bb82cc
Shryansh Parashar <Jaguar000212@gmail.com>
2024-06-18 13:31:57 +0530

    Created Message with a background
    Course 02/ Day 01

---
+++ app/src/main/java/com/jaguar/birthdaygreeting/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.jaguar.birthdaygreeting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.scaleIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jaguar.birthdaygreeting.ui.theme.BirthdayGreetingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BirthdayGreetingTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                        BirthdayGreetingImage("Jaguar")
+                    }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingText(from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Happy Birthday",
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            color = MaterialTheme.colorScheme.secondary,
+            textAlign = androidx.compose.ui.text.style.TextAlign.Center
+        )
+        Text(
+            text = "from $from",
+            fontSize = 50.sp,
+            color = MaterialTheme.colorScheme.tertiary,
+            textAlign = androidx.compose.ui.text.style.TextAlign.Right
+        )
+    }
+}
+
+@Composable
+fun BirthdayGreetingImage(from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        BirthdayGreetingText(
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayGreetingPreview() {
+    BirthdayGreetingTheme {
+        BirthdayGreetingImage("Jaguar")
+    }
+}
