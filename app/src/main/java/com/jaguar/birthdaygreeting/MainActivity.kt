26a8abefb6ba89f215483dd77a981d19409ae20d
Shryansh Parashar <Jaguar000212@gmail.com>
2024-06-20 02:53:00 +0530

    Course 02/ Day 02 -> Learnt about padding, align and some modifiers

--- app/src/main/java/com/jaguar/birthdaygreeting/MainActivity.kt
+++ app/src/main/java/com/jaguar/birthdaygreeting/MainActivity.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,9 +39,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthdayGreetingText(from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
         Text(
-            text = "Happy Birthday",
+            text = stringResource(R.string.wish),
             fontSize = 100.sp,
             lineHeight = 116.sp,
             color = MaterialTheme.colorScheme.secondary,
@@ -51,7 +54,8 @@ fun BirthdayGreetingText(from: String, modifier: Modifier = Modifier) {
             text = "from $from",
             fontSize = 50.sp,
             color = MaterialTheme.colorScheme.tertiary,
-            textAlign = androidx.compose.ui.text.style.TextAlign.Right
+            textAlign = androidx.compose.ui.text.style.TextAlign.Right,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
@@ -62,12 +66,11 @@ fun BirthdayGreetingImage(from: String, modifier: Modifier = Modifier) {
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
         )
         BirthdayGreetingText(
             from = from,
             modifier = Modifier
-                .fillMaxSize()
                 .padding(8.dp)
         )
     }
